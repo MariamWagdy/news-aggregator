fa97b6b7fae2b1ed68bf530a18aedfb31512b1d0
Mariam Wagdy <mariam.wagdy92@gmail.com>
2025-02-13 11:23:57 +0200

    Initialize User Authentication Module - Laravel Sanctum
    - Implemented user registration
    - Added login functionality
    - Enabled logout with token revocation
    - Implemented change password feature

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', [UserController::class, 'getUser']);
+    Route::put('/user/change-password', [UserController::class, 'changePassword']);
+});
